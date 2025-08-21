f7a769d795cac32c493361062bf64f6079f07da5
Diwakar Singh <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-08-21 18:10:57 +0530

    Update HeroText.jsx

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -3,6 +3,11 @@ const HeroText = () => {
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
       <div className="flex-col hidden md:flex c-space">
         <h1 className="text-4xl font-medium">Hey It's Diwakar</h1>
+        <div className="flex flex-col itmes-start">
+          <p className="text-5xl font-medium text-neutral-300">
+            A Developer <br /> Dedicated to Crafting
+          </p>
+        </div>
       </div>
     </div>
   );
