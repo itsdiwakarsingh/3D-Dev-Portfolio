33a8cbcc84dc926d5451f35f874940a61d570942
Diwakar <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-09-13 15:48:26 +0530

    updated home page

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,8 +1,22 @@
+import { Canvas } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
+import ParallaxBackground from "../components/ParallaxBackground";
+import { Astronaut } from "../components/Astronaut";
+
 const Hero = () => {
   return (
     <section className="flex itmes-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
       <HeroText />
+      <ParallaxBackground />
+      <figure
+        className="absolute inset-0"
+        style={{ width: "100vh", height: "100vh" }}
+      >
+        <Canvas>
+          <Astronaut />
+        </Canvas>
+        n
+      </figure>
     </section>
   );
 };
