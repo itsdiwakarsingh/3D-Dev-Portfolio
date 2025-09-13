b408b33789bda374ffe6ff89c647759aa596ea7d
Diwakar <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-09-13 20:31:26 +0530

    complete home page

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,8 +1,12 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Astronaut } from "../components/Astronaut";
 import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+import { Suspense } from "react";
+import { Loader } from "@react-three/drei";
+import { Float } from "@react-three/drei";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
@@ -15,14 +19,29 @@ const Hero = () => {
         style={{ width: "100vw", height: "100vh" }}
       >
         <Canvas camera={{ position: [0, 1, 3] }}>
-          <Astronaut
-            scale={isMobile && 0.23}
-            position={isMobile && [0, -1.5, 0]}
-          />
+          <Suspense fallback={<Loader />}>
+            <Float>
+              <Astronaut
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}
+              />
+            </Float>
+            <Rig />
+          </Suspense>
         </Canvas>
       </figure>
     </section>
   );
 };
+
+function Rig() {
+  return useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+      0.5
+    );
+  });
+}
 
 export default Hero;
