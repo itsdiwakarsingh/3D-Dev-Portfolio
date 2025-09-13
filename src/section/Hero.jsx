097be539ec41aff74bc15ef51c0c5348d2ae2488
Diwakar <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-09-13 19:32:11 +0530

    Update Hero.jsx

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -8,19 +8,23 @@ import { useMediaQuery } from "react-responsive";
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
-    <section className="flex itmes-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+    <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
       <ParallaxBackground />
       <figure
         className="absolute inset-0"
-        style={{ width: "100vh", height: "100vh" }}
+        style={{ width: "100vw", height: "100vh" }}
       >
         <Canvas camera={{ position: [0, 1, 3] }}>
-          <Astronaut
-            scale={isMobile && 0.23}
-            position={isMobile && [0, -1.5, 0]}
-          />
-          <OrbitControls />
+          <Suspense fallback={<Loader />}>
+            <Float>
+              <Astronaut
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}
+              />
+            </Float>
+            <Rig />
+          </Suspense>
         </Canvas>
       </figure>
     </section>
