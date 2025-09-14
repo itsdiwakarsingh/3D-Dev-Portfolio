d939f7fe68df295392d47a7e94743af32bb64beb
Diwakar <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-09-14 14:45:01 +0530

    Update About

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import Navbar from "./section/Navbar";
 import Hero from "./section/Hero";
-
+import About from "./section/About";
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-
 const About = () => {
   return (
-    <section className="c-space section-spacing">
+    <section className="c-space section-spacing" id="about">
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
-        <div className="flex items-end grid-default-color grid-1"></div>
-        <div className="flex items-end grid-default-color grid-2"></div>
-        <div className="flex items-end grid-default-color grid-3"></div>
-        <div className="flex items-end grid-default-color grid-4"></div>
-        <div className="flex items-end grid-default-color grid-5"></div>
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <p></p>
+        </div>
+        <div className="grid-default-color grid-2"></div>
+        <div className="grid-default-color grid-3"></div>
+        <div className=" grid-default-color grid-4"></div>
+        <div className=" grid-default-color grid-5"></div>
       </div>
     </section>
   );
